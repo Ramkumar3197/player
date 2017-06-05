ab188f308d5bdd4e24ad3fcf31d2ff95d5f2eec8
Ramkumar3197 <Ramkumar3197@users.noreply.github.com>
2017-06-05 03:25:56 -0700

    Update and rename odd.java to Pos.java

---
+++ Pos.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+public class Pos
+{
+  public static void main(String args[])
+  {
+    Scanner a=new Scanner(System.in);
+    int n;
+    n=a.nextInt();
+    if(n==0)
+    {
+      System.out.print("Zero");
+    }
+    else
+    {
+      if(n<o)
+      {
+        System.out.print("Negative");
+      }
+      else
+      {
+        System.out.print("Positive");
+      }
+    }
+  }
+}
+    
+  
